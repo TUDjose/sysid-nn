1f53bad680a8a0eb81b4f4721f15de2a2ace15db
TUDjose <J.B.PintodeMouraLeitedaCunha@student.tudelft.nl>
2024-05-04 00:43:40 +0200

    start OLS estimator

---
+++ least_squares.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import combinations_with_replacement
+plt.rcParams['text.usetex'] = True
+
+
+class LeastSquares:
+    def __init__(self, data, order, data_split):
+        self.Y, self.X = data
+        self.order = order
+        self.Ptrain, self.Pval, self.Ptest = data_split
+
+    def split_data(self):
+        pass
+
+    def regression_matrix(self, x):
+        N, states = x.shape
+        num_params = 1
+        A_list = [np.ones(N)]
+        for d in range(1, self.order+1):
+            combinations = combinations_with_replacement(range(states), d)
+            for combo in combinations:
+                poly_feature = np.ones(N)
+                for idx in combo:
+                    poly_feature *= x[:, idx]
+                A_list.append(poly_feature)
+                num_params += 1
+
+        A = np.column_stack(A_list)
+        return A, num_params
+
+    def OLS(self):
+        A, num_params = self.regression_matrix(self.X)
+        self.theta_hat = np.linalg.pinv(A) @ self.Y
+        self.y_hat = A @ self.theta_hat
+
+    def plot_regression(self, lims=(-0.2, 0.8)):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(self.X[:, 0], self.X[:, 1], self.y_hat, c='r', lw=1.3, label='OLS')
+        ax.plot(self.X[:, 0], self.X[:, 1], self.Y, c='k', lw=1.3, label='Measured')
+        ax.set_xlabel(r'$\alpha$ [rad]')
+        ax.set_ylabel(r'$\beta$ [rad]')
+        ax.set_zlabel(r'$C_m$ [-]')
+        ax.set_zlim(lims)
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+
+    def MSE(self):
+        return np.mean((self.Y - self.y_hat)**2)
+
+
+def order_influence(Y, X):
+    orders = np.arange(1, 21)
+    MSEs = []
+    for order in orders:
+        LS = LeastSquares(data=(Y, X), order=order, data_split=(0.7, 0.15, 0.15))
+        LS.OLS()
+        MSEs.append(LS.MSE())
+
+    plt.plot(orders, MSEs, marker='o')
+    plt.xlabel('Order')
+    plt.ylabel('MSE')
+    plt.grid()
+    plt.tight_layout()
+    plt.show()
--- read_data.py
+++ read_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 def train_data(filename='data/F16traindata_CMabV_2024.csv'):
     df = pd.read_csv(filename, header=None)
@@ -14,3 +14,10 @@ def validation_data(filename='data/F16validationdata_2024.csv'):
     alpha_val = df.iloc[:, 1].to_numpy()
     beta_val = df.iloc[:, 2].to_numpy()
     return Cm_val, alpha_val, beta_val
+
+def treat_data(data):
+    Y, X = data
+    idx = np.setdiff1d(np.arange(len(Y)), np.where(Y > 0.2))
+    Y = Y[idx]
+    X = X[idx]
+    return Y, X
